ca6a9515a98d64fc73af5b47181e94b8a01c7a3a
BevenStoner <sharjrwick@gmail.com>
2026-02-11 11:08:32 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
-class main(){
-    //Hi guys
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("Dataset\\reddit_dead_internet_analysis_2026.csv");
+        Scanner scan = new Scanner(file);
+        String data = "";
+        while(scan.hasNextLine())
+            data+=""+scan.nextLine();
+        System.out.println(data);
+        
+    }
 }
